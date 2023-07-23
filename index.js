ad9f16a5f9ec22daeb0dd2ad5daebf80d00aea60
Sayan-mitra1234 <119873741+Sayan-mitra1234@users.noreply.github.com>
2023-07-23 21:18:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app=require("./app")
 
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/product',{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect('mongodb+srv://swaggerUI:sayan1234@swaggerui.dedrimu.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true})
 
 app.listen(3005,()=>{console.log("Server running .........")})
